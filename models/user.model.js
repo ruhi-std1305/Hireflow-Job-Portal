21a13261e7ced114f225e41f94d9ca9d129bc644
Salsabil Tasnim <tasnim.shara3456@gmail.com>
2026-06-05 00:32:12 +0600

    backend spirint1

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    
+    role: {
+        type: String,
+        enum: ['seeker', 'employer', 'admin'],
+        required: true
+    },
+    // employer এর company name
+    companyName: {
+        type: String,
+        default: ""
+    },
+    profile: {
+        bio: { type: String },
+        skills: [{ type: String }],
+        resume: { type: String },
+        resumeOriginalName: { type: String },
+        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+        profilePhoto: {
+            type: String,
+            default: ""
+        }
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
