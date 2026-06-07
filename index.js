892fbe1be2d54b3e78307a4d7fe329c4f9c48a8f
Salsabil Tasnim <tasnim.shara3456@gmail.com>
2026-06-07 23:15:49 +0600

    fix: remove node_modules, .env, and duplicate middlewares from repo

--- index.js
+++ index.js
@@ -3,30 +3,19 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-
 import userRoute from "./routes/user.route.js";
-
-
 dotenv.config({});
-
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 const corsOptions = {
     origin: 'http://localhost:5173',
     credentials: true
 }
 app.use(cors(corsOptions));
-
 const PORT = process.env.PORT || 5000;
-
-
 app.use("/api/auth", userRoute);
-
-
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
