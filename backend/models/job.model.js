5df780c874aef8fd4fe507355b23a14753f04ad6
Salsabil45 <tasnim.shara3456@gmail.com>
2026-06-24 23:06:49 +0600

    Update job.model.js

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -6,7 +6,7 @@ const jobSchema = new mongoose.Schema({
     requirements: [{ type: String }],
     salary: { type: Number, required: true },
     location: { type: String, required: true },
-    jobType: { type: String, required: true }, // Full-time, Part-time, Remote
+    jobType: { type: String, required: true }, 
     experienceLevel: { type: Number, required: true },
     position: { type: Number, required: true },
     company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
